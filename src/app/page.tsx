6f6fbcf8277e35cb7651d57c04976f209da03c7c
Shakhriyor <shakhriyormamadaliev@gmail.com>
2024-06-20 15:52:52 +0500

    json to html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,17 @@ export default function Home() {
   return (
     <main>
       <ConvertFiles />
+      {/* <iframe
+        src="https://docs.google.com/viewer?url=https://calibre-ebook.com/downloads/demos/demo.docx&embedded=true"
+        width="100%"
+        height="600px"
+      ></iframe> */}
+      <iframe
+        className="mx-auto"
+        src="https://docs.google.com/viewer?url=https://calibre-ebook.com/downloads/demos/demo.docx&embedded=true"
+        height="1000"
+        width="800"
+      ></iframe>
     </main>
   );
 }
